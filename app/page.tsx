10d182af8f2c02709d744a76b4c36327e727c298
renuja99 <devmindadeco@gmail.com>
2024-08-12 15:09:26 +0530

    minor bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import BoardPage from "@/components/boardPage";
 import Infobar from "@/components/infobar";
 import { Toaster } from "sonner";
 
-export const revalidate = 5;
+
 
 export default function Home() {
 
